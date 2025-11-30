876405fc95e29534dd29ba471f34a1f112fbacfb
yugandharct <yugandhar.thottempudi@go.stcloudstate.edu>
2025-11-29 23:00:43 -0800

    Use dynamic base for GitHub Pages and local dev

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { HashRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
